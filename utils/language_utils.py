2b769f6fc40f0ce19796a8247dc385ddab7f8edf
amanraj74 <165486930+amanraj74@users.noreply.github.com>
2025-10-23 15:38:10 +0530

    Add missing utils folder + fix translator

--- utils/language_utils.py
+++ utils/language_utils.py
@@ -1,30 +1,38 @@
-"""Language utilities for 22 Indic languages"""
-
-LANGUAGES = {
-    "Hindi": "hin_Deva", "Bengali": "ben_Beng", "Tamil": "tam_Taml",
-    "Telugu": "tel_Telu", "Malayalam": "mal_Mlym", "Kannada": "kan_Knda",
-    "Marathi": "mar_Deva", "Gujarati": "guj_Gujr", "Odia": "ory_Orya",
-    "Punjabi": "pan_Guru", "Assamese": "asm_Beng", "Urdu": "urd_Arab",
-    "Maithili": "mai_Deva", "Sanskrit": "san_Deva", "Konkani": "gom_Deva",
-    "Nepali": "npi_Deva", "Sindhi": "snd_Deva", "Dogri": "doi_Deva",
-    "Manipuri": "mni_Mtei", "Bodo": "brx_Deva", "Kashmiri": "kas_Deva",
-    "Santali": "sat_Olck"
-}
-
-LANGUAGE_NAMES = {
-    "Hindi": "हिन्दी", "Bengali": "বাংলা", "Tamil": "தமிழ்", "Telugu": "తెలుగు",
-    "Malayalam": "മലയാളം", "Kannada": "ಕನ್ನಡ", "Marathi": "मराठी", "Gujarati": "ગુજરાતી",
-    "Odia": "ଓଡ଼ିଆ", "Punjabi": "ਪੰਜਾਬੀ", "Assamese": "অসমীয়া", "Urdu": "اردو",
-    "Maithili": "मैथिली", "Sanskrit": "संस्कृत", "Konkani": "कोंकणी", "Nepali": "नेपाली",
-    "Sindhi": "सिन्धी", "Dogri": "डोगरी", "Manipuri": "মৈতৈলোন্", "Bodo": "बर'",
-    "Kashmiri": "कॉशुर", "Santali": "ᱥᱟᱱᱛᱟᱲᱤ"
-}
-
-def get_language_code(language_name):
-    return LANGUAGES.get(language_name, "hin_Deva")
+"""Language utilities for 22 Indian languages"""
 
 def get_all_languages():
-    return list(LANGUAGES.keys())
+    """Return list of all 22 Indian languages"""
+    return [
+        "Hindi", "Bengali", "Tamil", "Telugu", "Malayalam", "Kannada",
+        "Marathi", "Gujarati", "Odia", "Punjabi", "Assamese", "Urdu",
+        "Maithili", "Sanskrit", "Konkani", "Nepali", "Sindhi", "Dogri",
+        "Manipuri", "Bodo", "Kashmiri", "Santali"
+    ]
 
-def get_language_display_name(lang):
-    return LANGUAGE_NAMES.get(lang, lang)
+def get_language_display_name(language):
+    """Get native script name for display"""
+    native_names = {
+        "Hindi": "हिन्दी",
+        "Bengali": "বাংলা",
+        "Tamil": "தமிழ்",
+        "Telugu": "తెలుగు",
+        "Malayalam": "മലയാളം",
+        "Kannada": "ಕನ್ನಡ",
+        "Marathi": "मराठी",
+        "Gujarati": "ગુજરાતી",
+        "Odia": "ଓଡ଼ିଆ",
+        "Punjabi": "ਪੰਜਾਬੀ",
+        "Assamese": "অসমীয়া",
+        "Urdu": "اردو",
+        "Maithili": "मैथिली",
+        "Sanskrit": "संस्कृतम्",
+        "Konkani": "कोंकणी",
+        "Nepali": "नेपाली",
+        "Sindhi": "سنڌي",
+        "Dogri": "डोगरी",
+        "Manipuri": "মৈতৈলোন্",
+        "Bodo": "बड़ो",
+        "Kashmiri": "कॉशुर",
+        "Santali": "ᱥᱟᱱᱛᱟᱲᱤ"
+    }
+    return native_names.get(language, language)
